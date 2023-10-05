6aeb956658f0874be7d175545bbdc735c277a797
iamtyga <139460707+iamtyga@users.noreply.github.com>
2023-10-05 14:41:30 +0100

    Add files via upload

---
+++ project2/javascipt.js
@@ -0,0 +1,12 @@
+
+    var y = prompt("how old are you"); 
+   var yearsLeft = 90 - y;
+   var years = yearsLeft * 1;
+   var months = yearsLeft * 12;
+   var weeks = yearsLeft * 52;
+   var days = yearsLeft * 365;
+   alert ("you have lived " +y+ " years and you still have " +years+ " years, " +days+ " days, "
+          +weeks+ " weeks and " +months+ " months left to reach age 90." );
+         
+   
+   
